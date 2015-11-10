e04c3ec8d78da28240f3765571cd8dda4603d180
Frederick Robinson <frederick.robinson@gmail.com>
2015-11-09 21:31:26 -0800

    second type

--- hw1_tests.py
+++ hw1_tests.py
@@ -7,18 +7,19 @@ class TestStringMethods(unittest.TestCase):
     def test_example(self):
         hospital_rankings = [[0, 1, 2], [2, 1, 0]]
         hospital_capacities = [1, 1]
-        student_rankings = [[0, 1], [0, 1], [0, 1]]
+        student_rankings = [[0, 1], [0, 1], [1, 0]]
 
         expected_output = [[0], [2]]
+        output = hw1.stable_matching(
+            hospital_rankings, hospital_capacities, student_rankings)
 
-        self.assertTrue(consistent(
-            hospital_rankings, hospital_capacities, student_rankings, expected_output))
-        self.assertTrue(first_kind(
-            hospital_rankings, hospital_capacities, student_rankings, expected_output))
-        self.assertTrue(second_kind(
-            hospital_rankings, hospital_capacities, student_rankings, expected_output))
-        self.assertEqual(
-            hw1.stable_matching(hospital_rankings, hospital_capacities, student_rankings), expected_output)
+        self.assertTrue(
+            consistent(hospital_rankings, hospital_capacities, student_rankings, output))
+        self.assertTrue(
+            first_kind(hospital_rankings, hospital_capacities, student_rankings, output))
+        self.assertTrue(
+            second_kind(hospital_rankings, hospital_capacities, student_rankings, output))
+        self.assertEqual(output, expected_output)
 
     def test_trickier(self):
         hospital_rankings = [[0, 1, 2], [0, 1, 2]]
@@ -26,15 +27,16 @@ class TestStringMethods(unittest.TestCase):
         student_rankings = [[0, 1], [0, 1], [0, 1]]
 
         expected_output = [[0], [1]]
+        output = hw1.stable_matching(
+            hospital_rankings, hospital_capacities, student_rankings)
 
-        self.assertTrue(consistent(
-            hospital_rankings, hospital_capacities, student_rankings, expected_output))
-        self.assertTrue(first_kind(
-            hospital_rankings, hospital_capacities, student_rankings, expected_output))
-        self.assertTrue(second_kind(
-            hospital_rankings, hospital_capacities, student_rankings, expected_output))
-        self.assertEqual(
-            hw1.stable_matching(hospital_rankings, hospital_capacities, student_rankings), expected_output)
+        self.assertTrue(
+            consistent(hospital_rankings, hospital_capacities, student_rankings, output))
+        self.assertTrue(
+            first_kind(hospital_rankings, hospital_capacities, student_rankings, output))
+        self.assertTrue(
+            second_kind(hospital_rankings, hospital_capacities, student_rankings, output))
+        self.assertEqual(output, expected_output)
 
 
 def consistent(hospital_rankings, hospital_capacities, student_rankings, output):
@@ -74,6 +76,30 @@ def first_kind(hospital_rankings, hospital_capacities, student_rankings, output)
 
 
 def second_kind(hospital_rankings, hospital_capacities, student_rankings, output):
+    assigned_students = set()
+
+    for choice in output:
+        assigned_students |= set(choice)
+
+    student_assignments = dict()
+    for i, choice in enumerate(output):
+        for student in choice:
+            if student in student_assignments:
+                return False
+            student_assignments[student] = i
+
+    for i, preference in enumerate(hospital_rankings):
+        choice = output[i]  # which students were chosen by hospital i
+        capacity = hospital_capacities[i]
+
+        for student in preference:
+            if student in choice:
+                capacity -= 1
+            if student in assigned_students and capacity > 0:
+                student_ranking = student_rankings[student]
+                if student_ranking.index(i) < student_ranking.index(student_assignments[student]):
+                    return False
+
     return True
 
 
