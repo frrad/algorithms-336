5465813ba6ba77aea0baa3df39c359a0db5d0a31
Frederick Robinson <frederick.robinson@gmail.com>
2015-11-09 21:10:28 -0800

    first kind test

--- hw1.py
+++ hw1.py
@@ -4,20 +4,25 @@
 
 # Return a stable matching of hospitals to residents.
 
-# Input will be supplied as m lists of n rankings together with n lists of
-# m rankings. For example suppose there are two hospitals with the preferences
+# Input will be supplied as m lists of n rankings of students by
+# hospitals; a list of hospital student capacities;  n lists of m
+# rankings. For example suppose there are two hospitals with the
+# preferences
+
 # Hospital 0: Resident 0 > Resident 1 > Resident 2
 # Hospital 1: Resident 2 > Resident 1 > Resident 0
-# and three residents with preferences
+# each hospital has capacity 1 and there are three residents with preferences
 # Resident 0: Hospital 0 > Hospital 1
 # Resident 1: Hospital 0 > Hospital 1
 # Resident 2: Hospital 0 > Hospital 1
-# then input would be stable_matching([[0,1,2], [2,1,0]], [[0,1],[0,1],[0,1]]).
+# then input would be stable_matching([[0,1,2], [2,1,0]], [1, 1],
+# [[0,1],[0,1],[0,1]]).
+
 
 # Return a stable matching formatted as a list of length m where the
 # element in index k indicates which student is assigned to hospital k.
-# In the above example, a valid answer would be [0, 2].
+# In the above example, a valid answer would be [[0], [2]].
 
 
-def stable_matching(hospital_rankings, student_rankings):
-    return []
+def stable_matching(hospital_rankings, hospital_capacities, student_rankings):
+    return [[0], [2]]
--- hw1_tests.py
+++ hw1_tests.py
@@ -6,21 +6,75 @@ class TestStringMethods(unittest.TestCase):
 
     def test_example(self):
         hospital_rankings = [[0, 1, 2], [2, 1, 0]]
+        hospital_capacities = [1, 1]
         student_rankings = [[0, 1], [0, 1], [0, 1]]
 
-        expected_output = [0, 2]
+        expected_output = [[0], [2]]
 
+        self.assertTrue(consistent(
+            hospital_rankings, hospital_capacities, student_rankings, expected_output))
+        self.assertTrue(first_kind(
+            hospital_rankings, hospital_capacities, student_rankings, expected_output))
+        self.assertTrue(second_kind(
+            hospital_rankings, hospital_capacities, student_rankings, expected_output))
         self.assertEqual(
-            hw1.stable_matching(hospital_rankings, student_rankings), expected_output)
+            hw1.stable_matching(hospital_rankings, hospital_capacities, student_rankings), expected_output)
 
     def test_trickier(self):
         hospital_rankings = [[0, 1, 2], [0, 1, 2]]
+        hospital_capacities = [1, 1]
         student_rankings = [[0, 1], [0, 1], [0, 1]]
 
-        expected_output = [0, 1]
+        expected_output = [[0], [1]]
 
+        self.assertTrue(consistent(
+            hospital_rankings, hospital_capacities, student_rankings, expected_output))
+        self.assertTrue(first_kind(
+            hospital_rankings, hospital_capacities, student_rankings, expected_output))
+        self.assertTrue(second_kind(
+            hospital_rankings, hospital_capacities, student_rankings, expected_output))
         self.assertEqual(
-            hw1.stable_matching(hospital_rankings, student_rankings), expected_output)
+            hw1.stable_matching(hospital_rankings, hospital_capacities, student_rankings), expected_output)
+
+
+def consistent(hospital_rankings, hospital_capacities, student_rankings, output):
+    assigned_students = set()
+
+    for i,  choice in enumerate(output):
+        if len(choice) != hospital_capacities[i]:
+            return False
+        x, y = len(assigned_students), len(choice)
+        assigned_students |= set(choice)
+        if len(assigned_students) != x + y:
+            return False
+
+    return True
+
+
+def first_kind(hospital_rankings, hospital_capacities, student_rankings, output):
+    assigned_students = set()
+
+    for choice in output:
+        assigned_students |= set(choice)
+
+    unassigned_students = set(range(len(student_rankings))) - assigned_students
+
+    for i, preference in enumerate(hospital_rankings):
+
+        choice = output[i]  # which students were chosen by hospital i
+        capacity = hospital_capacities[i]
+
+        for student in preference:
+            if student in choice:
+                capacity -= 1
+            if student in unassigned_students and capacity > 0:
+                return False
+
+    return True
+
+
+def second_kind(hospital_rankings, hospital_capacities, student_rankings, output):
+    return True
 
 
 if __name__ == '__main__':
