5a0b2e809ca19c0c950937fc040eb5343491bb9b
Frederick Robinson <frederick.robinson@gmail.com>
2015-11-10 21:19:13 -0800

    finished tests (hopefully)

--- hw1_tests.py
+++ hw1_tests.py
@@ -1,5 +1,7 @@
 import unittest
 import hw1
+import random
+from copy import deepcopy
 
 
 class TestStringMethods(unittest.TestCase):
@@ -9,26 +11,36 @@ class TestStringMethods(unittest.TestCase):
         hospital_capacities = [1, 1]
         student_rankings = [[0, 1], [0, 1], [1, 0]]
 
-        expected_output = [[0], [2]]
-        output = hw1.stable_matching(
+        self.execute_test(
             hospital_rankings, hospital_capacities, student_rankings)
-
-        self.assertTrue(
-            consistent(hospital_rankings, hospital_capacities, student_rankings, output))
-        self.assertTrue(
-            first_kind(hospital_rankings, hospital_capacities, student_rankings, output))
-        self.assertTrue(
-            second_kind(hospital_rankings, hospital_capacities, student_rankings, output))
-        self.assertEqual(output, expected_output)
 
     def test_trickier(self):
         hospital_rankings = [[0, 1, 2], [0, 1, 2]]
         hospital_capacities = [1, 1]
         student_rankings = [[0, 1], [0, 1], [0, 1]]
 
-        expected_output = [[0], [1]]
-        output = hw1.stable_matching(
+        self.execute_test(
             hospital_rankings, hospital_capacities, student_rankings)
+
+    def test_big(self):
+        hospital_rankings = [[5, 7, 2, 0, 6, 1, 4, 3], [5, 1, 3, 4, 0, 6, 2, 7], [
+            7, 1, 5, 3, 2, 4, 0, 6], [2, 3, 7, 5, 1, 4, 0, 6], [1, 6, 3, 2, 7, 4, 0, 5]]
+        hospital_capacities = [1, 2, 3, 1, 1]
+        student_rankings = [[1, 0, 3, 2, 4], [1, 4, 3, 2, 0], [3, 0, 1, 4, 2], [
+            0, 4, 1, 2, 3], [1, 2, 0, 3, 4], [4, 1, 0, 3, 2], [3, 0, 2, 1, 4], [1, 4, 0, 2, 3]]
+
+        self.execute_test(
+            hospital_rankings, hospital_capacities, student_rankings)
+
+    def test_random(self):
+        for i in xrange(100):
+            hospital_rankings, hospital_capacities, student_rankings = random_instance()
+            self.execute_test(
+                hospital_rankings, hospital_capacities, student_rankings)
+
+    def execute_test(self, hospital_rankings, hospital_capacities, student_rankings):
+        output = hw1.stable_matching(deepcopy(hospital_rankings), deepcopy(
+            hospital_capacities), deepcopy(student_rankings))
 
         self.assertTrue(
             consistent(hospital_rankings, hospital_capacities, student_rankings, output))
@@ -36,7 +48,6 @@ class TestStringMethods(unittest.TestCase):
             first_kind(hospital_rankings, hospital_capacities, student_rankings, output))
         self.assertTrue(
             second_kind(hospital_rankings, hospital_capacities, student_rankings, output))
-        self.assertEqual(output, expected_output)
 
 
 def consistent(hospital_rankings, hospital_capacities, student_rankings, output):
@@ -101,6 +112,27 @@ def second_kind(hospital_rankings, hospital_capacities, student_rankings, output
                     return False
 
     return True
+
+
+def random_instance():
+    x, num_hospitals = 3, 5
+    capacities = [random.randint(1, x) for i in xrange(num_hospitals)]
+    slots = sum(capacities)
+    num_students = random.randint(slots, 2 * slots)
+
+    hospital_rankings = []
+    for i in xrange(num_hospitals):
+        rank = range(num_students)
+        random.shuffle(rank)
+        hospital_rankings.append(rank)
+
+    student_rankings = []
+    for i in xrange(num_students):
+        rank = range(num_hospitals)
+        random.shuffle(rank)
+        student_rankings.append(rank)
+
+    return hospital_rankings, capacities, student_rankings
 
 
 if __name__ == '__main__':
